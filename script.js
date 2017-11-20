441c8bb7c522109095d70d4a4173e47b9b9d7ad7
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2017-11-20 10:45:05 -1000

    New Changes/Array

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-var x="";
+var Names= [
+  ''
+  ];
 function Display(){
 var x = document.getElementById('name').value;
+Names.push(x);
 var name = localStorage.setItem("UserName", x);
-window.alert(x);
+location.href="two.html";
 }
 
 function DisplayTheSave(){
 var z = localStorage.getItem("UserName");
 window.alert(z);
+var yz = document.getElementById('hey').innerHTML=Names.length;
 }
