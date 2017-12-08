cb8d25251a6d5a8da4c9b113768cb7ba726fa942
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2017-12-08 11:01:13 -1000

    Add files via upload

--- buyCode.js
+++ buyCode.js
@@ -1,1 +1,13 @@
- hey
+ var receipt = [];
+ var products = [
+   {
+   "prodO":"Castle Ravenloft",
+   "costO":60
+ }
+ ];
+ function buyProduct(){
+   var amount = document.getElementById('quantityOne').value;
+   var productCostTotal = amount * products[0].costO;
+   window.alert(productCostTotal);
+   
+ }
