3acf314fc9f86fb4a2dee726757eeb8e8d7a6b97
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2017-12-13 11:02:54 -1000

    Check Back later today

--- buyCode.js
+++ buyCode.js
@@ -8,13 +8,30 @@
  function buyProduct() {
    var amount = document.getElementById('quantityOne').value;
    var productCostTotal = amount * products[0].costO;
-   receipt.push(productCostTotal);
-   var STRINGIT = JSON.stringify
-   localStorage.setItem('product',receipt);
+   receipt.push(amount + "x" +productCostTotal);
+   var STRINGIT = JSON.stringify(receipt);
+   var ParseIt = JSON.parse(STRINGIT);
+   localStorage.setItem('product',ParseIt);
    var GetTotal = localStorage.getItem('product');
    window.alert(GetTotal);
-   
  }
- function totalOfEveryThing() {
-   
+ function nextPage() {
+   location.href = "total.html";
  }
+ function revealRecipt() {
+   var GetTotal = localStorage.getItem('product');
+   var STRINGIT = JSON.stringify(GetTotal);
+      var ParseIt = JSON.parse(STRINGIT);
+   document.getElementById('Test').innerHTML = GetTotal;
+   window.alert(GetTotal);
+ }
+ //check back later today because this is very very hard
+    for(i = 0;i<ParseIt;i++){
+   var newItem = document.createElement('li');
+   var Items = document.createTextNode(GetTotal[i]);
+   newItem.appendChild(Items);
+   var TotalReceipt = document.getElementsByTagName('ul')[0];
+   TotalReceipt.appendChild(newItem);
+   }
+   window.alert(GetTotal[2]);
+   var clearGetTotal = localStorage.clear();
