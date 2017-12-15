8472291237e759b86502f11ed2cd36143bbdf540
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2017-12-14 23:37:58 -1000

    talk to kam

--- buyCode.js
+++ buyCode.js
@@ -5,33 +5,25 @@
    "costO":60
  }
  ];
+ 
  function buyProduct() {
    var amount = document.getElementById('quantityOne').value;
    var productCostTotal = amount * products[0].costO;
    receipt.push(amount + "x" +productCostTotal);
-   var STRINGIT = JSON.stringify(receipt);
-   var ParseIt = JSON.parse(STRINGIT);
-   localStorage.setItem('product',ParseIt);
+   localStorage.setItem('product',receipt);
    var GetTotal = localStorage.getItem('product');
-   window.alert(GetTotal);
+   var stringit = JSON.stringify(receipt);
+   document.getElementById('test').innerHTML = receipt;
  }
+ 
  function nextPage() {
    location.href = "total.html";
  }
+ 
  function revealRecipt() {
    var GetTotal = localStorage.getItem('product');
    var STRINGIT = JSON.stringify(GetTotal);
-      var ParseIt = JSON.parse(STRINGIT);
+   var ParseIt = JSON.parse(STRINGIT);
    document.getElementById('Test').innerHTML = GetTotal;
    window.alert(GetTotal);
  }
- //check back later today because this is very very hard
-    for(i = 0;i<ParseIt;i++){
-   var newItem = document.createElement('li');
-   var Items = document.createTextNode(GetTotal[i]);
-   newItem.appendChild(Items);
-   var TotalReceipt = document.getElementsByTagName('ul')[0];
-   TotalReceipt.appendChild(newItem);
-   }
-   window.alert(GetTotal[2]);
-   var clearGetTotal = localStorage.clear();
