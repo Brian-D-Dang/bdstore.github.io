5628077155afd792a1bb31e102830e1369f090df
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2017-12-17 19:54:17 -1000

    Changed it to work!

--- buyCode.js
+++ buyCode.js
@@ -1,8 +1,19 @@
  var receipt = [];
+ var balance = 0;
  var products = [
    {
    "prodO":"Castle Ravenloft",
-   "costO":60
+   "costO":60,
+   "prod1":"Temple of Elemental Evil",
+   "cost1":60,
+   "prod2":"Legend of Drizzt",
+   "cost2":60,
+   "prod3":"Wrath of Ashardalon",
+   "cost3":60,
+   "prod4":"Tomb of Annihilation",
+   "cost4":60,
+   "prod5":"Catan",
+   "cost5":50
  }
  ];
  
@@ -10,25 +21,63 @@
    var amount = document.getElementById('quantityOne').value;
    var productCostTotal = amount * products[0].costO;
    receipt.push(productCostTotal);
-   var stringit = JSON.stringify(receipt);
+   window.alert(amount + "x" +productCostTotal);
+ }
+ 
+  function buyProductTwo() {
+   var amount = document.getElementById('quantityTwo').value;
+   var productCostTotal = amount * products[0].cost1;
+   receipt.push(productCostTotal);
+   window.alert(amount + "x" +productCostTotal);
+ }
+ 
+  function buyProductThree() {
+   var amount = document.getElementById('quantityThree').value;
+   var productCostTotal = amount * products[0].cost2;
+   receipt.push(productCostTotal);
+   window.alert(amount + "x" +productCostTotal);
+ }
+ 
+   function buyProductFour() {
+   var amount = document.getElementById('quantityFour').value;
+   var productCostTotal = amount * products[0].cost3;
+   receipt.push(productCostTotal);
+   window.alert(amount + "x" +productCostTotal);
+ }
+ 
+   function buyProductFive() {
+   var amount = document.getElementById('quantityFive').value;
+   var productCostTotal = amount * products[0].cost4;
+   receipt.push(productCostTotal);
+   window.alert(amount + "x" +productCostTotal);
+ }
+ 
+   function buyProductSix() {
+   var amount = document.getElementById('quantitySix').value;
+   var productCostTotal = amount * products[0].cost5;
+   receipt.push(productCostTotal);
    window.alert(amount + "x" +productCostTotal);
  }
  
  function nextPage() {
-   localStorage.setItem('product',receipt);
-   location.href = "total.html";
- }
- 
- function revealRecipt() {
-   var GetTotal = localStorage.getItem('product');
-   var Total = receipt.push(GetTotal);
-   var ParseIt = JSON.parse(receipt);
-   var balance = 0;
    for (i=0; i<receipt.length; i++){
      balance += receipt[i];
    }
+    if (balance === 0) {
+     window.alert("There are no items in your cart!");
+   }
+    else {
+   localStorage.setItem('product',balance);
+   location.href = "total.html";
+ }
+ }
+
+ 
+ function revealReceipt() {
+   var GetTotal = localStorage.getItem('product');
    var tax = 1.04712;
-   var GRANDTOTAL = balance * tax;
+   var GRANDTOTAL = GetTotal * tax;
    document.getElementById('Test').innerHTML = "GRANDTOTAL:" + ' ' +GRANDTOTAL.toFixed(2);
    window.alert(GRANDTOTAL.toFixed(2));
+   var clear = localStorage.clear();
  }
